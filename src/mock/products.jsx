baf4fb99e3fd4bbcf86e3b99b7e5f315357688d9
Jeferson <jeferson_014_17@hotmail.com>
2022-09-26 10:12:01 -0500

    se aumenta el stock para el producto lavadora

--- src/mock/products.jsx
+++ src/mock/products.jsx
@@ -1,7 +1,7 @@
 export const products = [
     { id: 1, nombre: 'Tv LG',categoria:{id:1,nombre:'ElectroDomestico'}, image: 'https://www.lg.com/cl/images/televisores/md05273581/gallery/medium01.jpg', Descripcion: 'Tv LG 50 pulgadas', precio: '2000', stock: 10 },
     { id: 2, nombre: 'Refrigeradora',categoria:{id:1,nombre:'ElectroDomestico'}, image: 'https://images.samsung.com/is/image/samsung/pe-ref-sbs-rs5300-rs27t5561b1-pe-frontjetblack-203517028?$650_519_PNG$', Descripcion: 'Refrigeradora Sansumg', precio: '1500', stock: 3 },
-    { id: 3, nombre: 'Lavadora',categoria:{id:1,nombre:'ElectroDomestico'}, image: 'https://oechsle.vteximg.com.br/arquivos/ids/1709424-1500-1500/1649755.jpg?v=637494974113530000', Descripcion: 'Lavadora LG', precio: '1700', stock: 0 },
+    { id: 3, nombre: 'Lavadora',categoria:{id:1,nombre:'ElectroDomestico'}, image: 'https://oechsle.vteximg.com.br/arquivos/ids/1709424-1500-1500/1649755.jpg?v=637494974113530000', Descripcion: 'Lavadora LG', precio: '1700', stock: 10 },
     { id: 4, nombre: 'Licuadora',categoria:{id:1,nombre:'ElectroDomestico'}, image: 'https://home.ripley.com.pe/Attachment/WOP_5/2019253949309/2019253949309_2.jpg', Descripcion: 'Licuadora LG', precio: '300', stock: 5 },
     { id: 5, nombre: 'Play Station',categoria:{id:1,nombre:'ElectroDomestico'}, image: 'https://cloudfront-us-east-1.images.arcpublishing.com/elcomercio/XPGZRX7LKJAL7LNLFOWMTZ7KEU.jpg', Descripcion: 'Play Station 5', precio: '2500', stock: 20 },
     { id: 6, nombre: 'Chaqueta Acolchada ',categoria:{id:2,nombre:'Hombre'}, image: 'https://m.media-amazon.com/images/I/81yTicQFo4L._AC_UL1500_.jpg', Descripcion: 'Chaqueta acolchada para varon', precio: '300', stock: 20 },
